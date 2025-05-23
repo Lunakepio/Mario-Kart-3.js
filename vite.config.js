afe75c0fbbe4f6ef3b6e94d5b87d33044ca24a9f
Lunakepio <moulinneufalex@gmail.com>
2025-05-23 13:36:39 +0200

    testing

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import glsl from 'vite-plugin-glsl'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), glsl()],
+  plugins: [react(), glsl(), VitePWA({
+       registerType: 'autoUpdate',
+       includeAssets: [ 'favicon.ico'],
+       manifest: {
+         name: 'Mario Kart 3.js',
+         short_name: 'MK3.JS',
+         start_url: '/',
+         display: 'standalone',
+         background_color: '#FF0000',
+         theme_color: '#FF0000',
+         icons: [
+           {
+             src: 'icon.webp',
+             sizes: '192x192',
+             type: 'image/webp'
+           },
+          
+         ]
+       }
+     })],
 })
